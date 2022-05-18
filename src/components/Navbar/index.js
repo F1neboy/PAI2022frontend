41352c4df7cb071fdf68c5f8e38c821c7f186c4d
Warda-99 <0nsidi0@gmail.com>
2022-05-18 18:22:31 +0200

    Create index.js

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {
+  Nav,
+  NavLink,
+  Bars,
+  NavMenu,
+  NavBtn,
+  NavBtnLink
+} from './NavbarElements';
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav className='nav_bar'>
+        <NavLink to='/'>
+          <h1>logo</h1>
+        </NavLink>
+        <Bars />
+        <NavMenu>
+
+          <NavLink to='/oferta' activeStyle>
+              Oferta
+          </NavLink>
+
+          <NavLink to='/rezerwacaja' activeStyle>
+              Rezerwacja
+          </NavLink>
+
+          <NavLink to='/about' activeStyle>
+              About
+          </NavLink>
+
+          <NavLink to='/contact-us' activeStyle>
+              Contact Us
+          </NavLink>
+
+        </NavMenu>
+        <NavBtn>
+          <NavBtnLink to='/signin'>Sign In</NavBtnLink>
+        </NavBtn>
+      </Nav>
+    </>
+  );
+};
+
+export default Navbar;
