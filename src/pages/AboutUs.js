b1a8978569a0a6202e87b8b5caf869dcf4cffac5
Warda-99 <0nsidi0@gmail.com>
2022-05-18 18:22:29 +0200

    Create AboutUs.js

---
+++ src/pages/AboutUs.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ImageSlider from "../components/slider/ImageSlider";
+import { SliderData } from "../components/slider/SliderData";
+
+function AboutUs() {
+    return <div className="about">
+
+        <h1>About Us</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus eu hendrerit arcu. Integer pulvinar rutrum mauris sit amet sollicitudin. Ut sollicitudin dolor in nisl semper finibus. Curabitur euismod ac nisi ut gravida. Maecenas non sagittis purus. Nulla ac posuere neque. Suspendisse gravida et ligula sodales molestie. Fusce mollis ut lacus vel tincidunt. Nam feugiat diam vel urna ullamcorper lobortis. Aenean efficitur efficitur justo, nec bibendum lacus condimentum sed. Aenean eu justo luctus, scelerisque mi a, gravida purus. Proin porta tellus eget nunc aliquam gravida. Cras imperdiet magna ligula, et laoreet dolor auctor ut.</p>
+
+        <ImageSlider slides={SliderData} /> 
+
+    </div>
+}
+
+export default AboutUs;
