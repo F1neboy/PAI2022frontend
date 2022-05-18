f1ce1b9fd0f0d20fba65f1bf55b877ddf7dbb482
Warda-99 <0nsidi0@gmail.com>
2022-05-18 18:22:06 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import React from 'react';
 import './App.css';
-import About from './components/Aboutus';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages';
+import AboutUs from './pages/AboutUs';
+import Oferta from './pages/oferta';
+import Rezerwacja from './pages/rezerwacja';
+import Contact from './pages/contact';
+
 
 function App() {
   return (
-    <div className="App">
-      <About></About>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' exact element={<Home/>} />
+        <Route path='/oferta' exact element={<Oferta/>} />
+        <Route path='/rezerwacaja' exact element={<Rezerwacja/>} />
+        <Route path='/about' exact element={<AboutUs/>} />
+        <Route path='/contact-us' exact element={<Contact/>} />
+      </Routes>
+    </Router>
   );
 }
 
