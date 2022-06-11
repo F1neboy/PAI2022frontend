1851438e2bbad83aa536ed733a47cdde0dec0f73
Argondt <aleksander.wieczarek1@gmail.com>
2022-06-12 00:55:31 +0200

    add text-shadow on homePage

--- src/components/homePageComponents/HeroElements.js
+++ src/components/homePageComponents/HeroElements.js
@@ -1,74 +1,75 @@
-import styled from 'styled-components';
-
+import styled from 'styled-components'
 
 export const HeroContainer = styled.div`
-    background-color: black;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding: 0 30px;
-    height: 800px;
-    position: relative;
-    z-index: 1;
+	background-color: black;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	padding: 0 30px;
+	height: 800px;
+	position: relative;
+	z-index: 1;
 
-    /* Add :before styles */
+	/* Add :before styles */
 `
 
 export const HeroBg = styled.div`
-   position: absolute;
-   top: 0;
-   right: 0;
-   bottom: 0;
-   left: 0;
-   width: 100%;
-   height: 100%;
-   overflow: hidden;
+	position: absolute;
+	top: 0;
+	right: 0;
+	bottom: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	overflow: hidden;
 `
 
 export const VideoBg = styled.video`
-   width: 100%;
-   height: 100%;
-   -o-object-fit: cover;
-   object-fit: cover;
-   background: #232a34;
+	width: 100%;
+	height: 100%;
+	-o-object-fit: cover;
+	object-fit: cover;
+	background: #232a34;
 `
 
 export const HeroContent = styled.div`
-   z-index: 3;
-   max-width: 1200px;
-   position: absolute;
-   padding: 8px 24px;
-   display: flex;
-   flex-direction: column;
-   align-items: center;
+	z-index: 3;
+	max-width: 1200px;
+	position: absolute;
+	padding: 8px 24px;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
 `
 
 export const HeroH1 = styled.h1`
-   color: #fff;
-   font-size: 48px;
-   text-align: center;
+	color: #fff;
+	font-size: 48px;
+	text-align: center;
+	text-shadow: 0.1em 0.1em 0.2em black;
 
-   @media screen and (max-width: 768px) {
-       font-size: 40px;
-   }
+	@media screen and (max-width: 768px) {
+		font-size: 40px;
+	}
 
-   @media screen and (max-width: 480px) {
-       font-size: 32px;
-   }
+	@media screen and (max-width: 480px) {
+		font-size: 32px;
+	}
 `
 
 export const HeroP = styled.p`
-   color: #fff;
-   margin-top: 24px;
-   font-size: 24px;
-   text-align: center;
-   max-width: 600px;
+	color: #fff;
+	margin-top: 24px;
+	font-size: 24px;
+	text-align: center;
+	max-width: 600px;
+	text-shadow: 0.1em 0.1em 0.2em black;
 
-   @media screen and (max-width: 768px) {
-       font-size: 24px;
-   }
+	@media screen and (max-width: 768px) {
+		font-size: 24px;
+	}
 
-   @media screen and (max-width: 480px) {
-       font-size: 18px;
-   }
+	@media screen and (max-width: 480px) {
+		font-size: 18px;
+	}
 `
